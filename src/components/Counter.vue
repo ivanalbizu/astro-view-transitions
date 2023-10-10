c5d0644401321e8cddad0840ae381cd95dd5e205
ivanalbizu <mariano-ivan.gonzalez-albizu@capgemini.com>
2023-10-10 16:37:42 +0200

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<template>
+  <button @click="count++" type="button"><slot></slot> {{count}}</button>
+</template>
+
+<style scoped>
+  button {
+    display: flex;
+    align-items: center;
+    border-radius: 2px;
+    font-size: 12px;
+    cursor: pointer;
+    color: #fff;
+    background-color: rebeccapurple;
+    padding: 0 4px;
+  }
+</style>
